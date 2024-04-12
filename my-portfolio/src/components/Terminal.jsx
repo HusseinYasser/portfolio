d6f328b33cdb09f880089956f27fce38a43279e4
HusseinYasser <husseinyasser388@gmail.com>
2024-04-13 00:59:42 +0200

    added scrolling to terminal

--- my-portfolio/src/components/Terminal.jsx
+++ my-portfolio/src/components/Terminal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const Terminal = () => {
 
@@ -7,6 +7,17 @@ const Terminal = () => {
     const [prevCommands, setPrevCommands] = useState([]);
 
     const [prevOutputs, setPrevOutputs] = useState([]);
+
+    const scrollableRef = useRef(null);
+
+    const inputRef = useRef(null);
+
+    useEffect(
+        () => {
+            scrollableRef.current.scrollTo(0, scrollableRef.current.scrollHeight);
+            inputRef.current.focus();
+        }
+    )
 
     const executeCommand = () =>
     {
@@ -21,50 +32,51 @@ const Terminal = () => {
         <div className = "bg-black rounded-lg border border-gray-200 p-4 w-1/2">
             {/* Terminal Header */}
             <div className = "flex mb-8">
-            <div className="flex space-x-1.5 items-center mr-auto">
-                    <div className="bg-green-500 w-3 h-3 rounded-full" />
-                    <div className="bg-yellow-500 w-3 h-3 rounded-full" />
-                    <div className="bg-red-500 w-3 h-3 rounded-full" />
+                <div className="flex space-x-1.5 items-center mr-auto">
+                        <div className="bg-red-500 w-3 h-3 rounded-full" />
+                        <div className="bg-yellow-500 w-3 h-3 rounded-full" />
+                        <div className="bg-green-500 w-3 h-3 rounded-full" />
                 </div>
                 <div className="text-blue-500 mr-auto text-lg">
                     <span className="text-green-500">visitor</span>@hussein-ebrahimPortfolio
                 </div>
-                
+                    
             </div>
+            <div className="max-h-96 overflow-y-auto" ref = {scrollableRef}>
+                {/* Hitory */}
+                {
+                    prevCommands.map((cmd, idx) => {
+                        return (
+                        <div className="flex-col mt-2" key={idx}>
+                            <div className="text-blue-500 ml-auto text-md w-full flex flex-wrap">
+                                <span className="text-green-500">visitor</span>@hussein-ebrahimPortfolio:<span className="text-lg">~$</span> 
+                                <span className="text-white ml-2"> {cmd} </span>
+                            </div>
+                            <div className="text-white">
+                                {prevOutputs[idx]}
+                            </div>
+                        </div>
+                        );
+                    })
+                }
 
-            {/* Hitory */}
-            {
-                prevCommands.map((cmd, idx) => {
-                    return (
-                    <div className="flex-col mt-2" key={idx}>
-                        <div className="text-blue-500 ml-auto text-md w-full flex flex-wrap">
-                            <span className="text-green-500">visitor</span>@hussein-ebrahimPortfolio:<span className="text-lg">~$</span> 
-                            <span className="text-white ml-2"> {cmd} </span>
-                        </div>
-                        <div className="text-white">
-                            {prevOutputs[idx]}
-                        </div>
+                {/* Terminal Writing Command */}
+                <div className="flex-col mt-2">
+                    <div className="text-blue-500 ml-auto text-md w-full flex flex-wrap">
+                        <span className="text-green-500">visitor</span>@hussein-ebrahimPortfolio:<span className="text-lg">~$</span>
+                        <input className="bg-black text-white border-none focus:outline-none pl-2 flex-grow" 
+                        spellCheck="false"
+                        value = {command}
+                        onChange={(event) => setCommand(event.target.value)}
+                        placeholder="help"
+                        onKeyDown={(event)=>{
+                            if(event.key === "Enter")
+                                executeCommand();
+                        }}
+                        ref = {inputRef} />
                     </div>
-                    );
-                })
-            }
-
-            {/* Terminal Writing Command */}
-            <div className="flex-col mt-2">
-                <div className="text-blue-500 ml-auto text-md w-full flex flex-wrap">
-                    <span className="text-green-500">visitor</span>@hussein-ebrahimPortfolio:<span className="text-lg">~$</span>
-                    <input className="bg-black text-white border-none focus:outline-none pl-2 flex-grow" 
-                    spellCheck="false"
-                    value = {command}
-                    onChange={(event) => setCommand(event.target.value)}
-                    placeholder="help"
-                    onKeyDown={(event)=>{
-                        if(event.key === "Enter")
-                            executeCommand();
-                    }} />
                 </div>
             </div>
-
         </div>
     </div>
   )
