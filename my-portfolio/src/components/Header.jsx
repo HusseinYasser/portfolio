54bc1f19aea3d0f6991152fd1677d591933b0341
HusseinYasser <husseinyasser388@gmail.com>
2024-04-13 15:14:59 +0200

    added a typewriter effect

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,14 +1,18 @@
 import MyImg from "../assets/Hussein.jpg";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import useTypewriter from "../hooks/useTypeWriter";
 
 const Header = () => {
+
+  const titleName = useTypewriter('Hussein Ebrahim', 200);
+
   return (
     <div className="ml-40 flex space-x-5 items-center pt-20">
       <img src={MyImg} alt="Hussein" className="rounded-full w-48 h-auto" />
       <div className="flex-col space-y-3">
         <div className="font-pixelfy text-white text-4xl">
-          Hussein Ebrahim
+          {titleName}
         </div>
         <div className="font-pixelfy text-white text-2xl">
           Software Engineer
---
+++ my-portfolio/src/hooks/useTypeWriter.jsx
@@ -0,0 +1,26 @@
+
+import  { useState, useEffect } from 'react';
+
+const useTypewriter = (text, speed = 200) => {
+    const [displayText, setDisplayText] = useState('');
+
+  useEffect(() => {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayText(text.substring(0, i + 1) + ( i < text.length-1? '|' : '' ));
+        i++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, speed);
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, [text, speed]);
+
+  return displayText;
+};
+
+export default useTypewriter;
